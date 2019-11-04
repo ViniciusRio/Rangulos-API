aaad7497454e4a0b821f9abf4bf568f52b0829ef
ViniciusRio <viniciustrave@gmail.com>
2019-11-04 15:39:18 -0400

    alterado rotas

--- htdocs/routes/api.php
+++ htdocs/routes/api.php
@@ -4,6 +4,7 @@ Route::post('register', 'AuthController@register');
 Route::post('login', 'AuthController@login');
 
 Route::get('events', 'EventController@index');
+Route::get('event/current', 'EventController@currentEvent');
 Route::get('event/{id}', 'EventController@show');
 Route::put('event/{id}', 'EventController@update');
 Route::post('events', 'EventController@store');
@@ -11,5 +12,10 @@ Route::delete('event/{id}', 'EventController@destroy');
 
 Route::get('event/{id}/guests', 'EventController@guests');
 
-Route::get('events/{id}/past', 'EventController@pastEvents');
+Route::get('events/past', 'EventController@pastEvents');
+
+
+
+Route::post('guests', 'GuestController@store');
+
 
