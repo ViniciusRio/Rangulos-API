49b53e5856b14cfdd1269f52ad0d8cdf4a76b61b
ViniciusRio <viniciustrave@gmail.com>
2019-11-06 09:52:56 -0400

    adicionado att date e fillable

--- htdocs/app/Event.php
+++ htdocs/app/Event.php
@@ -3,16 +3,37 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Event extends Model
 {
+    protected $fillable = [
+        'title',
+        'about',
+        'address',
+        'price',
+           'max_guests',
+        'start_date',
+        'end_date'
+    ];
+
     protected $dates = [
       'start_date',
       'end_date'
     ];
 
+    public function setStartDateAttribute($value)
+    {
+        $this->attributes['start_date'] = Carbon::parse($value);
+    }
 
-    public function userCreator() 
+    public function setEndDateAttribute($value)
+    {
+        $this->attributes['end_date'] = Carbon::parse($value);
+    }
+
+
+    public function userCreator()
     {
         return $this->beLongsTo(User::class, 'user_creator_id');
     }
